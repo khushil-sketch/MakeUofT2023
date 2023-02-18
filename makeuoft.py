8f8d40db8c5050427ee066dd70e5b48ecae95f30
ymoola <ymoola21@gmail.com>
2023-02-18 15:23:07 -0500

    Python File for audio conversion

---
+++ makeuoft.py
@@ -0,0 +1,17 @@
+import serial
+import wave
+import time
+
+ser = serial.Serial('COM3', 9600) # change 'COM3' to the serial port of your Arduino
+audio_file = wave.open('audio.wav', 'rb')
+sample_rate = audio_file.getframerate()
+
+while True:
+    audio_data = audio_file.readframes(1)
+    if not audio_data:
+        break
+    for sample in audio_data:
+        ser.write(bytes([sample]))
+        ser.flush()
+    delay = int(1000000 / sample_rate)
+    time.sleep(delay / 1000000)
